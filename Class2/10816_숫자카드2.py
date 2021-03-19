965ed00803b44556e4107894efb63d7d147631db
seoh02h <seok02h@khu.ac.kr>
2021-03-19 16:54:58 +0900

    Add files

--- Class2/10816_숫자카드2.py
+++ Class2/10816_숫자카드2.py
@@ -1,6 +1,18 @@
-n = int(input())
-s = list(map(int, input().split()))
-m = int(input())
-card = list(map(int, input().split()))
+import sys
 
-for i in range()  
+
+N = int(sys.stdin.readline())
+card_num = list(map(int, sys.stdin.readline().split()))
+
+M = int(input())
+target_list = list(map(int, sys.stdin.readline().split()))
+
+dict = {}
+
+for check_num in card_num:
+    if (check_num in dict):
+        dict[check_num] += 1
+    else:
+        dict[check_num] = 1
+
+print(' '.join(str(dict[target]) if target in dict else '0' for target in target_list))
