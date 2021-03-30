2b3c4fbe7ae64f6bb51e18f4c0848cd35d3c0b2a
seoh02h <seok02h@khu.ac.kr>
2021-03-30 11:27:29 +0900

    Add files

--- Lec02/2428_표절.py
+++ Lec02/2428_표절.py
@@ -1,12 +1,12 @@
 n = int(input())
-arㄱ = sorted(list(map(float,input().split())))
+arr = sorted(list(map(float,input().split())))
 answer = 0
 for i in range(n):
     l = i
     r = n
     while l<r:
         c = (l+r)//2
-        if arㄱ[i]>=arㄱ[c]*(0.9):
+        if arr[i]>=arr[c]*(0.9):
             l = c + 1
         else:
             r = c
